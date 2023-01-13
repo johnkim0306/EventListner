45f1a2d4924f008e9327e20ac183939067c2230d
johnkim0306 <mkcokr2176@gmail.com>
2023-01-13 03:28:51 -0700

    Fix Routing by adding Link component

--- pages/events/[cat]/index.js
+++ pages/events/[cat]/index.js
@@ -1,16 +1,17 @@
 import Image from "next/image";
+import Link from "next/link";
 
-const EventsCatPage = ({data}) => {
+const EventsCatPage = ({data, pageName}) => {
     return (
      <div>
-        <h1>Events in London</h1>
+        <h1>Events in {pageName}</h1>
         <div>
             {data.map((ev) => (
-                <a key={ev.id} href={`/events/${ev.city}/${ev.id}`}>
+                <Link key={ev.id} href={`/events/${ev.city}/${ev.id}`} passHref>
                     <Image width={300} height={300} alt={ev.title} src={ev.image} />
                     <h2> {ev.title} </h2>
                     <p> {ev.description} </p>
-                </a>
+                </Link>
             ))}
         </div>
     </div>
@@ -42,5 +43,5 @@ export async function getStaticProps(context) {
     const data = allEvents.filter(ev => ev.city ===id)
     console.log(data)
 
-    return {props: {data} };
+    return {props: {data, pageName: id} };
 }
--- pages/events/index.js
+++ pages/events/index.js
@@ -1,14 +1,15 @@
 import Image from 'next/image';
+import Link from "next/link";
 
 const EventsPage = ({data}) => {
   return (
     <div>
       <h1>Event page</h1>
       {data.map(ev => (
-        <a key={ev.id} href={`/events/${ev.id}`}>
+        <Link key={ev.id} href={`/events/${ev.id}`}>
           <Image src={ev.image} alt={ev.title} width={300} height={400} />
           <h2>{ev.title}</h2>
-        </a>
+        </Link>
       ))}
     </div>
   );
@@ -16,7 +17,7 @@ const EventsPage = ({data}) => {
 
 export default EventsPage;
 
-export async function getStaticProps() {
+export async function getStaticProps(context) {
   const { events_categories } = await import('/data/data.json')
   return {
     props: {
